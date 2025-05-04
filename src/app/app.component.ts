0044e7f6565030a00a04f6b5eb8f20bb3a3a36cd
ewdlop <ray810815@gmail.com>
2025-05-04 21:50:12 +0000

    create a greeting component and attatch it to app component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { GreetingComponent } from './components/greeting/greeting.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, GreetingComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'CodeSandbox';
